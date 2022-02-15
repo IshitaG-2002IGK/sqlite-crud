304ef1e0e57ad4f1bff4de2724f7904bca3ebce4
Ishita <ishitagops12@gmail.com>
2022-02-15 10:12:46 +0530

    city museum info

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Source:
 
 import sqlite3
 import json
+from sqlite3 import Error
 
 con = sqlite3.connect("museum.db")
 
@@ -15,19 +16,19 @@ try :
 
     cursor = con.cursor()
 
-    # cursor.execute( "DROP TABLE IF EXISTS MUSEUM")
+    cursor.execute( "DROP TABLE IF EXISTS MUSEUM")
 
-    # query = """ (CREATE TABLE MUSEUM(ID INT PRIMARY KEY NOT NULL, NAME CHAR(25),COUNTRY CHAR (20) NOT NULL)"""
+    query = """ (CREATE TABLE MUSEUM(ID INT PRIMARY KEY NOT NULL, NAME CHAR(25),COUNTRY CHAR (20) NOT NULL)"""
 
     con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(1, 'Smith Institution', 'Washington')")
 
     con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(2, 'Le Louvre', 'Paris')")
 
-
     con.commit()
     print("DONE")
 
-except :
+except Error as e :
+    print(e)
     print("ERROR")
     con.rollback()
 

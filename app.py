5eaf1eb64529719c1459b9eed3eae0a3af87cca6
Ishita <ishitagops12@gmail.com>
2022-02-15 09:24:58 +0530

    inserting values into table

--- app.py
+++ app.py
@@ -17,7 +17,14 @@ cursor.execute( "DROP TABLE IF EXISTS MUSEUM")
 
 query = """ (CREATE TABLE MUSEUM(ID INT PRIMARY KEY NOT NULL, NAME CHAR(25),COUNTRY CHAR (20) NOT NULL)"""
 
-con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY")
+con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(1, 'Smith Institution', 'Washington')")
+
+con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(1, 'Smith Institution', 'Washington')")
+
+# con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(1, 'Smith Institution', 'Washington')")
+
+# con.execute("INSERT INTO MUSEUM(ID, NAME, COUNTRY)""VALUES(1, 'Smith Institution', 'Washington')")
+
 
 con.commit()
 
